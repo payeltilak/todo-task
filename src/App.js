dcc66b63904fe31f91a7046780113cc6031effe0
unknown <you@example.com>
2022-06-30 23:38:41 +0600

    add to navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Footer from './HomePage/Footer';
+import Navbar from './HomePage/Navbar';
+import { Routes,Route } from "react-router-dom";
+import Home from './HomePage/Home';
+import TodoList from './HomePage/TodoList';
+import CompletedList from './HomePage/CompletedList';
+import Calender from './HomePage/Calender';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/todolist" element={<TodoList></TodoList>}></Route>
+        <Route path="/completedlist" element={<CompletedList></CompletedList>}></Route>
+        <Route path="/calender" element={<Calender></Calender>}></Route>
+        
+        
+
+
+
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      // "dark",
+      "cupcake"
+    ]
+   
+  },
+  plugins: [require("daisyui")],
+}
